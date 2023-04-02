78402ef548e1038a957fc5cd87a773e65f58ad7a
Mika Repo <mikeorap@gmail.com>
2023-04-02 12:09:08 +0300

    Osa 1 tehtävät 1.1 ja 1.2

---
+++ src/App.js
@@ -0,0 +1,76 @@
+/*const Hello = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>Hello {props.name}, you are {props.age} years old</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const nimi = 'Pekka'
+  const ika = 10
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10}/>
+      <Hello name={nimi} age={ika}/>
+    </>
+  )
+}*/
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.header}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.part1} exercises={props.exercises1}/>
+      <Part part={props.part2} exercises={props.exercises2}/>
+      <Part part={props.part3} exercises={props.exercises3}/>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return(
+    <div>
+      <p>Number of exercises {props.exercisesTotal}</p>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return(
+    <div>
+      <p>
+        {props.part} {props.exercises}
+      </p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header header={course}/>
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
+      <Total exercisesTotal={exercises1 + exercises2 + exercises3}/>
+    </div>
+  )
+}
+
+export default App
